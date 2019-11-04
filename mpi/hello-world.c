8382d785a7479f7dc2c62eb4d9e668ffddb5b342
Shubham Maurya <code.shaurya@gmail.com>
2019-11-04 20:25:08 +0530

    Update hello-world.c

--- mpi/hello-world.c
+++ mpi/hello-world.c
@@ -1,14 +1,18 @@
 #include <mpi.h>
 #include <stdio.h>
- 
-int main (int argc, char* argv[])
+#define  MASTER  	0
+
+int main(int argc, char *argv[])
 {
   int rank, size;
- 
-  MPI_Init (&argc, &argv);      /* starts MPI */
-  MPI_Comm_rank (MPI_COMM_WORLD, &rank);        /* get current process id */
-  MPI_Comm_size (MPI_COMM_WORLD, &size);        /* get number of processes */
-  printf( "Hello world from process %d of %d\n", rank, size );
+
+  MPI_Init(&argc, &argv);               /* starts MPI */
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); /* get current process id */
+  MPI_Comm_size(MPI_COMM_WORLD, &size); /* get number of processes */
+  printf("Hello world from process %d of %d\n", rank, size);
+  if (rank == MASTER)
+    printf("MASTER: Number of MPI tasks is: %d\n", size);
+
   MPI_Finalize();
   return 0;
 }
